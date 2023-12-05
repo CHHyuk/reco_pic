701682d5ad153902e837fc5e09097d6efe2ad542
CHHyuk <gksgur1038@naver.com>
2023-12-05 20:58:47 +0900

    라우터 버전 관리, 탑바 라우터 적용

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import "./Topbar.css";
 import { StyledButton } from '../StyledButton';
+import { useNavigate } from 'react-router-dom';
 
 export default function Topbar() {
+    const navigate = useNavigate();
+
+    const mainButtonClick = () => {
+      navigate('/')
+    }
+
+    const rankButtonClick = () => {
+      navigate('/rank')
+    }
+
     return (
       <topbar className="top-bar">
         <div className="web-title">
           WebTitle
         </div>
         <div className="navigator">
-          <StyledButton>
+          <StyledButton onClick={mainButtonClick}>
              Main
           </StyledButton>
-          <StyledButton>
+          <StyledButton onClick={rankButtonClick}>
              Ladderboard
           </StyledButton>
+          <div></div>
         </div>
       </topbar>
     );
