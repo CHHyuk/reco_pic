fd1e3b1e70624fc04d4697247f0ee855514a8657
CHHyuk <gksgur1038@naver.com>
2023-12-05 20:55:54 +0900

    router 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Topbar from './components/Topbar/Topbar.js'
+import Gamepage from './components/Gamepage/Gamepage.js'
+import Mainpage from './components/Mainpage/Mainpage.js'
+import Rankingpage from './components/Rankingpage/Rankingpage.js'
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Topbar/>
+    <Router>
+      <Topbar/>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/game" element={<Gamepage />} />
+          <Route path="/rank" element={<Rankingpage />} />
+        </Routes>
+    </Router>
   );
 }
 
