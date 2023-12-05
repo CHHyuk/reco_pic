199bf7b26ac37b0414cf84a8768de6bb0f6e699a
CHHyuk <gksgur1038@naver.com>
2023-12-05 16:22:50 +0900

    상단 바 꾸미기

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,6 +1,21 @@
+import React from 'react';
+import "./Topbar.css";
+import { StyledButton } from '../StyledButton';
+
 export default function Topbar() {
     return (
-      <div>
-      </div>
+      <topbar className="top-bar">
+        <div className="web-title">
+          WebTitle
+        </div>
+        <div className="navigator">
+          <StyledButton>
+             Main
+          </StyledButton>
+          <StyledButton>
+             Ladderboard
+          </StyledButton>
+        </div>
+      </topbar>
     );
   }
