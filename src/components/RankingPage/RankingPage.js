50ed0a9c17152caa45e5510e9b02b70ef1e59e6c
CHHyuk <gksgur1038@naver.com>
2023-12-17 13:01:27 +0900

    랭킹페이지 구현

--- src/components/RankingPage/RankingPage.js
+++ src/components/RankingPage/RankingPage.js
@@ -1,9 +1,79 @@
-import React from 'react';
-import './RankingPage.css'
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import './RankingPage.css';
+import NotificationComponent from '../GamePage/NotificationComponent';
+
 export default function RankingPage() {
-    return(
-        <div className="ranking_container">
-            ----
+  const [rankingData, setRankingData] = useState([]);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [notificationSeverity, setNotificationSeverity] = useState('success');
+  const [notificationMessage, setNotificationMessage] = useState('Copy success!');
+
+  useEffect(() => {
+    const fetchRankingData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/api/ranking/top20`);
+        setRankingData(response.data);
+      } catch (error) {
+        console.log('Error loading ranking data', error);
+      }
+    };
+
+    fetchRankingData();
+  }, []);
+
+  const handleCopyUrl = (url) => {
+    navigator.clipboard.writeText(url).then(() => {
+      setSnackbarOpen(true);
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false); 
+  };
+
+
+  const handleWheel = (e) => {
+    const container = document.querySelector(".ranking_container");
+    const containerScrollPosition = container.scrollLeft;
+
+    const scrollSpeedMultiplier = 10;
+
+    container.scrollTo({
+      top: 0,
+      left: containerScrollPosition + (e.deltaY * scrollSpeedMultiplier),
+      behavior: 'smooth'
+    });
+  };
+
+  return (
+    <div className="ranking_container" onWheel={handleWheel}>
+      {rankingData.map((item, index) => (
+        <div key={index} className='rank_img'>
+          <img src={`https://picsum.photos/id/${item.id}/500/500`} alt={`Image ${index + 1}`} />
+          <div className='rank_info'>
+            <p className='rank_title'>Rank: {index + 1}</p>
+            <p
+              className='rank_url'
+              onClick={() => handleCopyUrl(`https://picsum.photos/id/${item.id}/500/500`)}
+            >
+              URL: {`https://picsum.photos/id/${item.id}/500/500`}
+            </p>
+            <p className='rank_score'>Score: {item.score}</p>
+          </div>
         </div>
-    );
+      ))}
+      <NotificationComponent
+        open={snackbarOpen}
+        onClose={handleSnackbarClose}
+        severity={notificationSeverity}
+        message={notificationMessage}
+      />
+    </div>
+  );
 }
